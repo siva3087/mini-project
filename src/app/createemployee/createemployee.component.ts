a3f330625f71f4461af5cdc9e7a222e296400615
siva3087 <sivanagar13@gmail.com>
2024-11-28 18:49:08 +0530

    create employee

--- src/app/createemployee/createemployee.component.ts
+++ src/app/createemployee/createemployee.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { EmployeeService } from '../employee.service';
+import { FormControl, FormControlName, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-createemployee',
@@ -6,5 +8,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./createemployee.component.css']
 })
 export class CreateemployeeComponent {
+ constructor(private _employeeService:EmployeeService){}
 
+ public employeeForm:FormGroup = new FormGroup(
+  {name: new FormControl(),
+    company: new FormControl(),
+    role: new FormControl(),
+    package: new FormControl(),
+    email: new FormControl(),
+    dob: new FormControl(),
+    address: new FormGroup({
+    addressLine: new FormControl(),
+    city: new FormControl(),
+    state: new FormControl(),
+    pincode: new FormControl(),
+    })
+ })
+ submit(){
+  console.log(this.employeeForm);
+  this._employeeService.createemployee(this.employeeForm.value).subscribe(
+    (data:any) =>{
+      alert(" created sucessfully");
+    },
+    (err:any) =>{
+      alert("Creation Failed");
+    }
+  )
 }
+
+ }
+
