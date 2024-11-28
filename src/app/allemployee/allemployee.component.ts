641da84ee4fb986f3442025002437a5b8d349052
siva3087 <sivanagar13@gmail.com>
2024-11-28 17:33:28 +0530

    table

--- src/app/allemployee/allemployee.component.ts
+++ src/app/allemployee/allemployee.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { EmployeeService } from '../employee.service';
 
 @Component({
   selector: 'app-allemployee',
@@ -6,5 +7,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./allemployee.component.css']
 })
 export class AllemployeeComponent {
+
+  employees:any =[];
+  constructor(private _employeeService:EmployeeService){
+    _employeeService.getemployees().subscribe(
+      (data:any)=>{
+        this.employees =data;
+        console.log(this.employees)
+      },
+      (err:any)=>{
+        alert("internal server error")
+      }
+
+    )
+  }
 
 }
